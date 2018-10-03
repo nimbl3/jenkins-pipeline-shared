cac3b1147cd9e7eab9ce9def579f46d9b42641b5
Issarapong Poesua <dekablade01@gmail.com>
2018-10-03 15:11:43 +0700

    add fallback to current commit message

--- vars/containsCommitMessage.groovy
+++ vars/containsCommitMessage.groovy
@@ -1,5 +1,5 @@
 def call(String message) {
-  def lastCommitMessage = env.COMMIT_MESSAGE
+  def lastCommitMessage = env['COMMIT_MESSAGE'].isEmpty() ? getCommitMessage() : env['COMMIT_MESSAGE']
   if (lastCommitMessage.contains(message)) {
     echo "Found \'${message}\' in git latest commit message"
     return true
