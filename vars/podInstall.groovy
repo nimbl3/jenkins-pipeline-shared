bfb4935aee52bf9e9b43f00650d9a65e0b84db33
Jason Nam <contact@jasonnam.com>
2019-07-18 14:43:53 +0700

    Use map argument

--- vars/podInstall.groovy
+++ vars/podInstall.groovy
@@ -1,3 +1,7 @@
-def call(String... options) {
-  sh 'summon/bin pod install' + options.inject('') { r, v -> r + " --$v" }
+def call(Map args = [:]) {
+  String options = ''
+  if (args.projectDirectory != null) {
+    options = " --project-directory=${args.projectDirectory}"
+  }
+  sh 'summon/bin pod install' + options
 }
