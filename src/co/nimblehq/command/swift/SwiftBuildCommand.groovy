4ada3f385fb8a79a6f6d5f1999ac99ed8765d0aa
Jason Nam <contact@jasonnam.com>
2019-07-23 15:10:48 +0700

    Add whitelines

--- src/co/nimblehq/command/swift/SwiftBuildCommand.groovy
+++ src/co/nimblehq/command/swift/SwiftBuildCommand.groovy
@@ -3,6 +3,7 @@ package co.nimblehq.command.swift
 class SwiftBuildCommand extends SwiftCommand {
 
   final String action = 'build'
+
   Map options
 
   Map options() {
--- src/co/nimblehq/command/swift/SwiftTestCommand.groovy
+++ src/co/nimblehq/command/swift/SwiftTestCommand.groovy
@@ -3,6 +3,7 @@ package co.nimblehq.command.swift
 class SwiftTestCommand extends SwiftCommand {
 
   final String action = 'build'
+
   Map options
 
   Map options() {
