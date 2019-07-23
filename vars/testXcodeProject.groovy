2707e7ae468a568dd2dc933a3b2e632ecae3ff4f
Jason Nam <contact@jasonnam.com>
2019-07-23 16:02:24 +0700

    Use correct class

--- vars/testXcodeProject.groovy
+++ vars/testXcodeProject.groovy
@@ -3,7 +3,7 @@ import co.nimblehq.command.fastlane.FastlaneRunCommand
 import co.nimblehq.command.fastlane.FastlaneCommandSerializer
 
 def call(Map parameters = [:]) {
-  FastlaneRunCommand fastlaneRunCommand = new FastlaneCommand(fastlaneAction: 'scan', parameters: parameters)
+  FastlaneRunCommand fastlaneRunCommand = new FastlaneRunCommand(fastlaneAction: 'scan', parameters: parameters)
   FastlaneCommandSerializer commandSerializer = new FastlaneCommandSerializer()
   sh Summon.bin(commandSerializer.serialize(fastlaneRunCommand))
 }
