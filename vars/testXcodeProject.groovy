5abd35860e67dd3cf3ac1ef0330c07e8d1686f39
Jason Nam <contact@jasonnam.com>
2019-07-23 17:56:06 +0700

    Rename to options

--- vars/testXcodeProject.groovy
+++ vars/testXcodeProject.groovy
@@ -3,7 +3,7 @@ import co.nimblehq.command.fastlane.FastlaneRunCommand
 import co.nimblehq.command.fastlane.FastlaneCommandSerializer
 
 def call(Map parameters = [:]) {
-  FastlaneRunCommand fastlaneRunCommand = new FastlaneRunCommand(fastlaneAction: 'scan', parameters: parameters)
+  FastlaneRunCommand fastlaneRunCommand = new FastlaneRunCommand(fastlaneAction: 'scan', options: parameters)
   FastlaneCommandSerializer commandSerializer = new FastlaneCommandSerializer()
   sh Summon.bin(commandSerializer.serialize(fastlaneRunCommand))
 }
