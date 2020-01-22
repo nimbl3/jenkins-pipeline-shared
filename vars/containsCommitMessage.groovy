d229a5bbf8cf8b44a288b3053c1b788d0381b603
Issarapong Poesua <dekablade01@gmail.com>
2020-01-22 18:06:52 +0700

    update calling env

--- vars/containsCommitMessage.groovy
+++ vars/containsCommitMessage.groovy
@@ -1,5 +1,6 @@
 def call(String message) {
-  def lastCommitMessage = env['COMMIT_MESSAGE'].isEmpty() ? getCommitMessage() : env['COMMIT_MESSAGE']
+  def commitMessage = env.COMMIT_MESSAGE
+  def lastCommitMessage = commitMessage.isEmpty() ? getCommitMessage() : commitMessage
   if (lastCommitMessage.contains(message)) {
     echo "Found \'${message}\' in git latest commit message"
     return true
